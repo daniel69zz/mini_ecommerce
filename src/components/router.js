8e0fe7e74a22b5cf063e0da5f60bda374d36740c
daniel69zz <luisdanielrojascaceres@gmail.com>
2025-10-20 09:12:43 -0400

    nosotros add

--- src/components/router.js
+++ src/components/router.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/Home.vue";
+import Nosotros from "./Nosotros.vue";
 
 export default createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", component: Home }],
+  routes: [
+    { path: "/", component: Home },
+    { path: "/nosotros", component: Nosotros },
+  ],
 });
